16871930bd3cef566d8f4a6ec3c7026ffa5df008
teslabyte <stevanovicmateja04@gmail.com>
2022-09-19 13:00:54 +0200

    Initial commit -Added Home page controller class -Added Home page html file - index.html -Added Home page css file

---
+++ src/main/java/mateja/bazamusterija/BazaMusterijaApplication.java
@@ -0,0 +1,14 @@
+package mateja.bazamusterija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BazaMusterijaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazaMusterijaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mateja/bazamusterija/BazaMusterijaApplicationTests.java
@@ -0,0 +1,13 @@
+package mateja.bazamusterija;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazaMusterijaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
