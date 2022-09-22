f6484995432524e52a2af497ac4e702b144eda08
teslabyte <15111613+teslabyte@users.noreply.github.com>
2022-09-22 12:47:40 +0200

    Added Custom HTML element class CustomerInfoBox Tested generating CustomerInfoBox on the site programmatically

---
+++ src/main/resources/static/js/customer.js
@@ -0,0 +1,35 @@
+class CustomerInfoBox extends HTMLElement{
+    constructor() {
+        super();
+    }
+
+    render(){
+        var ime = this.getAttribute('ime') || undefined;
+        var brojTelefona = this.getAttribute('brojtelefona') || undefined;
+        var opisKvara = this.getAttribute('opiskvara') || undefined;
+        var adresa = this.getAttribute('adresa') || undefined;
+        var cena = this.getAttribute('cena') || undefined;
+        this.innerHTML = 'Ime: ' + ime + '<br/>' +
+            'Opis kvara: ' + opisKvara + '<br/>' +
+            'Adresa: ' + adresa + '<br/>' +
+            'Cena: ' + cena + '<br/>' +
+            'Broj telefona: ' + brojTelefona + '<br/>';
+    }
+
+    connectedCallback(){
+        if(!this.rendered){
+            this.render();
+            this.rendered = true;
+        }
+    }
+
+    static get observedAttributes(){
+        return ['ime','brojtelefona','opiskvara','adresa','cena'];
+    }
+
+    attributeChangedCallback(nekitekst){
+        this.render();
+    }
+}
+
+customElements.define('cib-element', CustomerInfoBox);
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,1 +1,28 @@
-console.log(musterije)
+//console.log(musterije)
+var testing = document.getElementById("losd");
+
+musterije.forEach((item)=>{
+    var div = document.createElement("div");
+    var cust = document.createElement("cib-element");
+    div.classList.add("cust-box-div");
+    cust.classList.add("cust-box");
+    cust.setAttribute('ime', item.ime);
+    cust.setAttribute('brojtelefona', item.brojTelefona);
+    cust.setAttribute('opiskvara', item.opisKvara);
+    cust.setAttribute('adresa', item.adresa);
+    cust.setAttribute('cena', item.cena);
+    div.appendChild(cust);
+    testing.appendChild(div);
+})
+
+
+//let lista = document.getElementById("testLista");
+//musterije.forEach((item)=>{
+    //let m = document.createElement("li");
+   // m.innerText = musterijaToString(item)
+   // lista.appendChild(m);
+//})
+
+//function musterijaToString(m){
+  //  return m.ime + "," + m.brojTelefona;
+//}
